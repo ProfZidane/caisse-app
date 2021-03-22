8f52c945c0ca70d90efb3a5621376802fba4925d
ProfZidane <madazada0@gmail.com>
2021-03-22 15:00:05 +0100

    add login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loadingFinish = true;
   title = 'caissapp';
+
+  ngOnInit(): void {
+    /*setInterval(() => {
+      this.loadingFinish = false;
+    }, 2000);*/
+    throw new Error('Method not implemented.');
+  }
+
 }
+
