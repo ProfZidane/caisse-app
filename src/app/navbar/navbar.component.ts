7798dc1118c4fa032d9701dcb1933f9fc737d8ed
unknown <madazada0gmail.com>
2021-05-27 09:45:12 +0200

    add logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,15 +7,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+userConnected;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('caissier') !== null) {
+      this.userConnected = JSON.parse(localStorage.getItem('caissier'));
+    }
   }
 
   logout() {
-    this.router.navigateByUrl('/');
     // remove data in localstorage
+    localStorage.removeItem('word_token');
+    this.router.navigateByUrl('/');
   }
 
 }
